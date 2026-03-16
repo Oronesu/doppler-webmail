ed235fb05372e0eb89ba40465f71e80f55e515f4
Oronesu <einn.dh@gmail.com>
2026-03-16 08:57:35 +0100

    Edit Mailview.tsx transfert et réponse

--- client/src/components/MailView.tsx
+++ client/src/components/MailView.tsx
@@ -1,14 +1,35 @@
+interface MailViewProps {
+  subject: string;
+  from: string;
+  to: string;
+  body: string;
+  onReply: () => void;
+  onForward: () => void;
+}
 
-const MailView = ({ body }: { body: string }) => {
+const MailView = ({ subject, from, to, body, onReply, onForward }: MailViewProps) => {
   return (
-    <div className="p-3">
-      <h5>📨 Contenu du message</h5>
-      <div dangerouslySetInnerHTML={{ __html: body }} />
+    <div className="mailview-container">
+
+      {/* BARRE D’EN-TÊTE */}
+      <div className="mailview-header">
+        <h4 className="mailview-subject">{subject}</h4>
+
+        <div className="mailview-meta">
+          <p><strong>De :</strong> {from}</p>
+          <p><strong>À :</strong> {to}</p>
+        </div>
+
+        <div className="mailview-actions">
+          <button className="btn btn-outline-primary" onClick={onReply}>↩ Répondre</button>
+          <button className="btn btn-outline-secondary" onClick={onForward}>⤳ Transférer</button>
+        </div>
+      </div>
+
+      {/* CONTENU DU MAIL */}
+      <div className="mailview-body" dangerouslySetInnerHTML={{ __html: body }} />
     </div>
   );
 };
 
-
-
 export default MailView;
-
